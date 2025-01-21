5fc22bcdd943fdc4946ba5c7f37f53259a2a8e66
yffty09 <fujisawa_yuta@ftgroup.co.jp>
2025-01-21 01:13:21 +0000

    Assistant checkpoint: Add user creation functionality to admin interface
    Assistant generated file changes:
    - app/controllers/admin/users_controller.rb: Add new user functionality
    - app/views/admin/users/index.html.erb: Add user creation form
    
    ---
    
    User prompt:
    
    ギフトコード一覧にユーザ作成機能を実装してください。

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,24 @@
+
 class Admin::UsersController < Admin::BaseController
-    def index
+  def index
+    @users = User.all.includes(:gift_codes)
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, success: 'ユーザーを作成しました'
+    else
       @users = User.all.includes(:gift_codes)
+      flash.now[:error] = 'ユーザー作成に失敗しました'
+      render :index, status: :unprocessable_entity
     end
   end
-  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
+  end
+end
